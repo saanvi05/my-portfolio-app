f690476816b3c84bc7539a94721f341582fcbba4
saanvi05 <saanvi1705@gmail.com>
2025-09-16 18:23:43 +0530

    Position change in skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -26,6 +26,26 @@ function Skills() {
       <h2 className="section-title">My Skills</h2>
       <div className="skills-grid">
         <div className="skill-item">
+          <FaFigma className="skill-icon" />
+          <p>Figma</p>
+        </div>
+
+        <div className="skill-item">
+          <SiAdobexd className="skill-icon" />
+          <p>Adobe XD</p>
+        </div>
+
+        <div className="skill-item">
+          <SiAdobephotoshop className="skill-icon" />
+          <p>Adobe Photoshop</p>
+        </div>
+
+        <div className="skill-item">
+          <SiAdobeillustrator className="skill-icon" />
+          <p>Adobe Illustrator</p>
+        </div>
+
+        <div className="skill-item">
           <FaHtml5 className="skill-icon" />
           <p>HTML5</p>
         </div>
@@ -45,33 +65,14 @@ function Skills() {
           <p>React</p>
         </div>
 
-        <div className="skill-item">
+        {/* <div className="skill-item">
           <FaNodeJs className="skill-icon" />
           <p>Node.js</p>
-        </div>
+        </div> */}
 
         <div className="skill-item">
           <FaGitAlt className="skill-icon" />
           <p>Git</p>
-        </div>
-        <div className="skill-item">
-          <FaFigma className="skill-icon" />
-          <p>Figma</p>
-        </div>
-
-        <div className="skill-item">
-          <SiAdobexd className="skill-icon" />
-          <p>Adobe XD</p>
-        </div>
-
-        <div className="skill-item">
-          <SiAdobeillustrator className="skill-icon" />
-          <p>Adobe Illustrator</p>
-        </div>
-
-        <div className="skill-item">
-          <SiAdobephotoshop className="skill-icon" />
-          <p>Adobe XD</p>
         </div>
       </div>
     </section>
