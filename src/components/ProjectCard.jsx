0463ae88db8eeb3771d3003d91ff938053bc0af9
saanvi05 <saanvi1705@gmail.com>
2025-09-16 16:10:37 +0530

    Updated the link in the project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 // src/components/ProjectCard.jsx
-import React from 'react';
-import './ProjectCard.css';
-import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'; // Icons for links
+import React from "react";
+import "./ProjectCard.css";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"; // Icons for links
 
 // This component receives 'props' (properties) from the Projects component.
 // We are "destructuring" them into individual variables like { image, title, ... }
@@ -15,12 +15,26 @@ function ProjectCard({ image, title, description, githubLink, liveLink }) {
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
         <div className="card-links">
-          <a href={githubLink} target="_blank" rel="noopener noreferrer" className="card-link">
-            <FaGithub /> Code
-          </a>
-          <a href={liveLink} target="_blank" rel="noopener noreferrer" className="card-link">
-            <FaExternalLinkAlt /> Live Demo
-          </a>
+          {githubLink && (
+            <a
+              href={githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card-link"
+            >
+              <FaGithub /> Code
+            </a>
+          )}
+          {liveLink && (
+            <a
+              href={liveLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card-link"
+            >
+              <FaExternalLinkAlt /> Live Demo
+            </a>
+          )}
         </div>
       </div>
     </div>
